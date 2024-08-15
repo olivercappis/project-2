c4fc56d55c9c21da267a6f572142183221262711
Oliver Cappis <Oliver_Cappis1@baylor.edu>
2024-08-15 08:16:22 -0500

    got server to run

--- models/index.js
+++ models/index.js
@@ -2,7 +2,7 @@ const User = require('./User')
 const Connection = require('./Connection')
 
 
-User.belongsToMany(User, { through: Connection })
+User.belongsToMany(User, { through: Connection, as: 'user' })
 
 module.exports = {
     User,
