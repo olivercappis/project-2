122522b1efb33e76e6a6423fe55a6d260e040534
Oliver Cappis <Oliver_Cappis1@baylor.edu>
2024-08-18 10:54:09 -0500

    updaitng branch

--- public/js/connectbtn.js
+++ public/js/connectbtn.js
@@ -1,19 +1,21 @@
-const connectBtnHandler = async (event) => {
-    event.preventDefault();
-  
-      const response = await fetch('/api/connection', {
-        method: 'POST',
-        body: JSON.stringify({ user_id, connected_to_id }),
-        headers: { 'Content-Type': 'application/json' },
-      });
-  
-      if (response.ok) {
-        console.log('Connection added!');
-      } else {
-        alert('Connection failed.');
-      }
-    }
+const connectBtnHandler = async (event, req) => {
+  event.preventDefault();
 
-document
-  .querySelector('#connect-btn')
-  .addEventListener('click', connectBtnHandler);
+  console.log(req.session.user_id)
+
+  const response = await fetch('/api/connection', {
+    method: 'POST',
+    body: JSON.stringify({ user_id, connected_to_id }),
+    headers: { 'Content-Type': 'application/json' },
+  });
+
+  if (response.ok) {
+    console.log('Connection added!');
+  } else {
+    alert('Connection failed.');
+  }
+}
+
+document.querySelectorAll('.connect-btn').forEach(button => {
+  button.addEventListener('click', connectBtnHandler);
+});
